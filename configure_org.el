547d43d2daa5905e52405a7106073d37b446c156
Doga Yuksel <dogayuksel@gmail.com>
2017-12-03 13:02:24 +0100

    Modify org-mode dependent imports to use :after keyword

--- configure_org.el
+++ configure_org.el
@@ -16,7 +16,8 @@
    ("C-c a" . org-agenda)
    ("C-c c" . org-capture)
    ("C-c b" . org-iswitchb))
-  :defines (org-capture-templates
+  :defines (org-export-initial-scope
+            org-capture-templates
             org-mobile-directory
             org-mobile-encryption-password
             my/org-mobile-encryption-password
@@ -51,8 +52,8 @@
           org-mobile-directory "~/Dropbox/Apps/MobileOrg"
           org-mobile-encryption-password my/org-mobile-encryption-password
           org-mobile-inbox-for-pull "~/Dropbox/.org/from-mobile.org"
-          org-mobile-use-encryption t)
-    (setq org-agenda-custom-commands
+          org-mobile-use-encryption t
+          org-agenda-custom-commands
           '(("g" . "GTD contexts")
             ("go" "Office" tags-todo "@office")
             ("gc" "Computer" tags-todo "@hacking")
@@ -76,19 +77,6 @@
             ))
     (add-hook 'org-mode-hook 'visual-line-mode)
     (add-hook 'latex-mode-hook 'visual-line-mode)
-    (use-package visual-fill-column
-      :init
-      (setq-default
-       visual-fill-column-center-text t
-       visual-fill-column-width 90)
-      :config
-      (add-hook 'org-mode-hook 'visual-fill-column-mode)
-      (add-hook 'org-agenda-mode-hook 'visual-fill-column-mode)
-      (add-hook 'latex-mode-hook 'visual-fill-column-mode))
-    (use-package org-bullets
-      :init
-      (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-      )
     (defun my/buffer-face-mode-variable ()
       "Set font to a variable width fonts in current buffer."
       (interactive)
@@ -99,47 +87,64 @@
       (set-face-attribute 'org-code nil :inherit 'fixed-pitch)
       (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
       (buffer-face-mode))
-    (add-hook 'org-mode-hook 'my/buffer-face-mode-variable)
-    (use-package ox-latex
-      :ensure org
-      :config
-      (progn
-        (add-to-list 'org-latex-classes
-                     '("koma-article"
-                       "\\documentclass{scrartcl}"
-                       ("\\section{%s}" . "\\section*{%s}")
-                       ("\\subsection{%s}" . "\\subsection*{%s}")
-                       ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                       ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                       ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-        (add-to-list 'org-latex-classes
-                     '("koma-book"
-                       "\\documentclass{scrbook}"
-                       ("\\chapter{%s}" . "\\chapter*{%s}")
-                       ("\\section{%s}" . "\\section*{%s}")
-                       ("\\subsection{%s}" . "\\subsection*{%s}")
-                       ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                       ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                       ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-        (add-to-list 'org-latex-classes
-                     '("scrlttr"
-                       "\\documentclass[11pt]{scrlttr2}\n
+    (add-hook 'org-mode-hook 'my/buffer-face-mode-variable)))
+
+(use-package visual-fill-column
+  :init
+  (setq-default
+   visual-fill-column-center-text t
+   visual-fill-column-width 90)
+  :after (org)
+  :config
+  (add-hook 'org-mode-hook 'visual-fill-column-mode)
+  (add-hook 'org-agenda-mode-hook 'visual-fill-column-mode)
+  (add-hook 'latex-mode-hook 'visual-fill-column-mode))
+
+(use-package org-bullets
+  :init
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  :after (org))
+
+(use-package ox-latex
+  :ensure org
+  :after (org)
+  :config
+  (progn
+    (add-to-list 'org-latex-classes
+                 '("koma-article"
+                   "\\documentclass{scrartcl}"
+                   ("\\section{%s}" . "\\section*{%s}")
+                   ("\\subsection{%s}" . "\\subsection*{%s}")
+                   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                   ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                   ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+    (add-to-list 'org-latex-classes
+                 '("koma-book"
+                   "\\documentclass{scrbook}"
+                   ("\\chapter{%s}" . "\\chapter*{%s}")
+                   ("\\section{%s}" . "\\section*{%s}")
+                   ("\\subsection{%s}" . "\\subsection*{%s}")
+                   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                   ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                   ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+    (add-to-list 'org-latex-classes
+                 '("scrlttr"
+                   "\\documentclass[11pt]{scrlttr2}\n
                \\usepackage[utf8]{inputenc}\n
                \\usepackage[T1]{fontenc}\n
                \\usepackage{xcolor}"
-                       ("\\section{%s}" . "\\section*{%s}")
-                       ("\\subsection{%s}" . "\\subsection*{%s}")
-                       ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                       ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                       ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-        (setq org-latex-caption-above nil
-              org-latex-pdf-process
-              '("latexmk -pdflatex='pdflatex -interaction nonstopmode' -pdf -bibtex -f  %f"))
-        )
-      )
-    (use-package ox-md
-      :ensure org
-      )))
+                   ("\\section{%s}" . "\\section*{%s}")
+                   ("\\subsection{%s}" . "\\subsection*{%s}")
+                   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                   ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                   ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+    (setq org-latex-caption-above nil
+          org-latex-pdf-process
+          '("latexmk -pdflatex='pdflatex -interaction nonstopmode' -pdf -bibtex -f  %f"))))
+
+(use-package ox-md
+  :ensure org
+  :after (org))
 
 (use-package org-gcal
   :defines (org-gcal-client-id
@@ -152,8 +157,10 @@
    "172076857986-m651ifu3fbc51usuet841lvp3m8t11g7.apps.googleusercontent.com"
    org-gcal-client-secret my/org-gcal-client-secret
    org-gcal-file-alist
-   '(("dogayuksel@gmail.com" . "~/Dropbox/.org/gcal.org"))))
-(add-hook 'org-agenda-mode-hook (lambda () (org-gcal-sync)))
+   '(("dogayuksel@gmail.com" . "~/Dropbox/.org/gcal.org")))
+  :after (org)
+  :config
+  (add-hook 'org-agenda-mode-hook (lambda () (org-gcal-sync))))
 
 (setq reftex-default-bibliography
       (quote
