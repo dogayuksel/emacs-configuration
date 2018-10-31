4722afef1398ea04e94ce8aa7723b16cd9ced5af
Doga Yuksel <doga@Dogas-MBP.fritz.box>
2018-10-31 17:39:24 +0100

    override evil bindings in neotree mode

--- basics.el
+++ basics.el
@@ -249,10 +249,19 @@
 (use-package all-the-icons)
 
 (use-package neotree
-  :after (all-the-icons)
+  :after (all-the-icons evil)
   :bind ("C-c n" . neotree)
   :config
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+  (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+  (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+  (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle))
 
 (use-package undo-tree
   :config
