67eafc24e24417e4d63855f496a1d8a71208d210
Doga Yuksel <dogayuksel@gmail.com>
2019-07-23 22:45:41 +0200

    add org-brain

--- configure_org.el
+++ configure_org.el
@@ -150,6 +150,21 @@
    "k" 'evil-previous-visual-line)
   :delight)
 
+(use-package org-brain
+  :init
+  (setq org-brain-path "~/Dropbox/.org/brain")
+  ;; For Evil users
+  (with-eval-after-load 'evil
+    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  :config
+  (setq org-id-track-globally t)
+  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+  (push '("b" "Brain" plain (function org-brain-goto-end)
+          "* %i%?" :empty-lines 1)
+        org-capture-templates)
+  (setq org-brain-visualize-default-choices 'all)
+  (setq org-brain-title-max-length 12))
+
 (use-package visual-fill-column
   :init
   (setq visual-fill-column-center-text t
