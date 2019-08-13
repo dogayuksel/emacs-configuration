d84fa05cd32234137150f84c761060d572c69468
Doga Yuksel <dogayuksel@gmail.com>
2019-08-13 17:52:46 +0200

    add dummy secrets file to repo

---
+++ .emacs_secrets_dummy.el
@@ -0,0 +1,10 @@
+;;; .emacs --- My emacs configuration file
+
+;;; Commentary:
+;;; Sensitive data
+
+;;; Code:
+
+(setq my/org-mobile-encryption-password ""
+      my/org-gcal-client-secret ""
+      my/org-gcal-client-id "")
