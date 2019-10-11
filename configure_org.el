ab3f0eb442ada03e9939a3617a8f223ab80324b9
Doga Yuksel <dyuksel@Dogas-MacBook-Pro.local>
2019-10-11 12:24:05 +0200

    load org-brain only when brain index exists

--- configure_org.el
+++ configure_org.el
@@ -151,19 +151,22 @@
   :delight)
 
 (use-package org-brain
+  :if (file-exists-p "~/Dropbox/.org/brain/index.org")
   :init
-  (setq org-brain-path "~/Dropbox/.org/brain")
-  ;; For Evil users
-  (with-eval-after-load 'evil
-    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  (progn
+    (setq org-brain-path "~/Dropbox/.org/brain")
+    ;; For Evil users
+    (with-eval-after-load 'evil
+      (evil-set-initial-state 'org-brain-visualize-mode 'emacs)))
   :config
-  (setq org-id-track-globally t)
-  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
-  (push '("b" "Brain" plain (function org-brain-goto-end)
-          "* %i%?" :empty-lines 1)
-        org-capture-templates)
-  (setq org-brain-visualize-default-choices 'all)
-  (setq org-brain-title-max-length 20))
+  (progn
+    (setq org-id-track-globally t)
+    (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+    (push '("b" "Brain" plain (function org-brain-goto-end)
+            "* %i%?" :empty-lines 1)
+          org-capture-templates)
+    (setq org-brain-visualize-default-choices 'all)
+    (setq org-brain-title-max-length 20)))
 
 (use-package visual-fill-column
   :init
